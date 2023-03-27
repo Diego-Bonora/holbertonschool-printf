50dfdff16fcecd509b3cbc3f5eeb769e0e56da77
Diego Bonora <bonoradi@gmail.com>
2023-03-27 14:30:59 -0700

    inclution of all functions for the correct operation of the program

---
+++ main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+typedef struct format
+{
+	char op;
+	int (*f)(va_list ap);
+} format_t;
+
+int printf_c(va_list ap);
+int printf_s(va_list ap);
+
+#endif
