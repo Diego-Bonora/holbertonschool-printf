f49aa900a6ce5f42b62b2fd62b4f3cb0b4f916f8
Diego Bonora <bonoradi@gmail.com>
2023-03-27 14:30:17 -0700

    functions for printing string and char with the % in the printf

---
+++ functions.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+
+int printf_c(va_list ap)
+{
+	char letter;
+
+	letter = va_arg(ap, int);
+	write(1, &letter, 1);
+	return (0);
+}
+
+int printf_s(va_list ap)
+{
+	char *string;
+
+	string = va_arg(ap, char*);
+	write(1, string, strlen(string));
+	return (0);
+}
