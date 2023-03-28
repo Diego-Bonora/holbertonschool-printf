618a500ea95352bc59e059694a740fb4b500cb65
Diego Bonora <bonoradi@gmail.com>
2023-03-28 14:14:50 -0700

    modification of diferent function prototypes and addition of description for the structure

--- main.h
+++ main.h
@@ -9,22 +9,29 @@
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-void print_number(int n);
-void print_unsigned(unsigned int n);
+int print_number(int n, int len);
+int print_unsigned_num(unsigned int n, int len);
 int _strlen(char *s);
 void print_rev(char *s);
+
+/**
+ * struct format - structure for calling diferent functions
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
 
 typedef struct format
 {
 	char op;
-	int (*f)(va_list ap);
+	int (*f)(va_list ap, int total);
 } format_t;
 
-int printf_c(va_list ap);
-int printf_s(va_list ap);
-int printf_same(va_list ap);
-int printf_d(va_list ap);
-int printf_u(va_list ap);
-int printf_b(va_list ap);
+int printf_c(va_list ap, int total);
+int printf_s(va_list ap, int total);
+int printf_same(va_list ap, int total);
+int printf_d(va_list ap, int total);
+int printf_u(va_list ap, int total);
+int printf_b(va_list ap, int total);
 
 #endif
