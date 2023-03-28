49d6a8dd132f5f79dd28f5a75382272c10908d02
Diego Bonora <bonoradi@gmail.com>
2023-03-28 11:04:49 -0700

    new prototypes of functions added

--- main.h
+++ main.h
@@ -9,6 +9,10 @@
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+void print_number(int n);
+void print_unsigned(unsigned int n);
+int _strlen(char *s);
+void print_rev(char *s);
 
 typedef struct format
 {
@@ -19,5 +23,8 @@ typedef struct format
 int printf_c(va_list ap);
 int printf_s(va_list ap);
 int printf_same(va_list ap);
+int printf_d(va_list ap);
+int printf_u(va_list ap);
+int printf_b(va_list ap);
 
 #endif
