a036eba7f5205acaeee3fcb98e2ae0289c64f838
Diego Bonora <bonoradi@gmail.com>
2023-03-28 11:00:27 -0700

    new functions to print different values

--- functions.c
+++ functions.c
@@ -20,7 +20,7 @@ int printf_s(va_list ap)
 	char *string;
 
 	string = va_arg(ap, char*);
-	write(1, string, strlen(string));
+	write(1, string, _strlen(string));
 	return (0);
 }
 
@@ -30,5 +30,42 @@ int printf_same(va_list ap)
 
 	(void)ap;
 	write(1, &letter, 1);
+	return (0);
+}
+
+int printf_d(va_list ap)
+{
+	int num;
+
+	num = va_arg(ap, int);
+	print_number(num);
+	return (0);
+}
+
+int printf_u(va_list ap)
+{
+	unsigned int num;
+
+	num = va_arg(ap, unsigned int);
+	print_unsigned(num);
+	return (0);
+}
+
+int printf_b(va_list ap)
+{
+	int num;
+	int cant = 0;
+	char *str;
+
+	str = malloc(sizeof(char));
+	num = va_arg(ap, int);
+	while (num > 0)
+	{
+		str[cant] = ((num % 2) + '0');
+		num /= 2;
+		str = realloc(str, sizeof(char) * (sizeof(str) + 1));
+		cant++;
+	}
+	print_rev(str);
 	return (0);
 }
