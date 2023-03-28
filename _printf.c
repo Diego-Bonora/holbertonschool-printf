9033255e554db6dbde1de61b21f965d9f7b382cf
Diego Bonora <bonoradi@gmail.com>
2023-03-28 16:25:34 -0700

    fixed double printing when 2 functions are next to each other

--- _printf.c
+++ _printf.c
@@ -8,6 +8,7 @@
 
 int _printf(const char *format, ...)
 {
+	int flag;
 	va_list ap;
 	int total_len = 0;
 	int len;
@@ -21,30 +22,32 @@ int _printf(const char *format, ...)
 		{'i', printf_d},
 		{'u', printf_u},
 		{'b', printf_b},
+		{'x', printf_h},
+		{'X', printf_h},
+		{'o', printf_o},
 		{'\0', NULL}
 	};
 
 	va_start(ap, format);
 	for (len = 0; format[len]; len++)
 	{
+		flag = 0;
 		if (format[len] == '%')
-		{
-			len2 = 0;
+		{	len2 = 0;
 			while (operators[len2].op)
 			{
 				if (format[len + 1] == operators[len2].op)
-				{
-					total_len = operators[len2].f(ap, total_len);
+				{	total_len = operators[len2].f(ap, total_len);
 					len++;
-				}
-				len2++;
-			}
-		}
+					flag = 1;
+			       		break; }
+				len2++; }
+			if (flag == 0)
+			{	_putchar(format[len]);
+				total_len++; } }
 		else
-		{
-			_putchar(format[len]);
-			total_len++;
-		}
+		{	_putchar(format[len]);
+			total_len++; }
 	}
 	va_end(ap);
 	return (total_len);
