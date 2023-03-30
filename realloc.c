6517bf56bdd6ee6754a69d5f1dcbde2db0879593
Diego Bonora <bonoradi@gmail.com>
2023-03-30 11:50:29 -0700

    handmade realloc function

---
+++ realloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _realloc - realocates memory
+ * @ptr: pointer input
+ * @size: new size
+ * Return: pointer to new space
+ */
+
+char *_realloc(char *ptr, size_t size)
+{
+	int len = 0;
+	char *new;
+
+	new = malloc(size);
+	if (!new)
+		exit(-1);
+	while(ptr[len])
+	{
+		new[len] = ptr[len];
+		len++;
+	}
+	new[len] = '\n';
+	free(ptr);
+	return (new);
+}
