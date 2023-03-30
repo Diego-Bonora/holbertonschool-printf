a4f3c81428dfaeca64946c8867c92fde9ec02f83
Diego Bonora <bonoradi@gmail.com>
2023-03-30 11:47:25 -0700

    prototypes modified for inputing an int pointer insted of an int

--- main.h
+++ main.h
@@ -6,6 +6,7 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <string.h>
+#include <limits.h>
 
 int _putchar(char c);
 int _printf(const char *format, ...);
@@ -13,6 +14,8 @@ int print_number(int n, int len);
 int print_unsigned_num(unsigned int n, int len);
 int _strlen(char *s);
 void print_rev(char *s);
+char *_realloc(char *ptr, size_t size);
+int (*get_function(char format))(va_list ap, int *total);
 
 /**
  * struct format - structure for calling diferent functions
@@ -24,15 +27,15 @@ void print_rev(char *s);
 typedef struct format
 {
 	char op;
-	int (*f)(va_list ap, int total);
+	int (*f)(va_list ap, int *total);
 } format_t;
 
-int printf_c(va_list ap, int total);
-int printf_s(va_list ap, int total);
-int printf_same(va_list ap, int total);
-int printf_d(va_list ap, int total);
-int printf_u(va_list ap, int total);
-int printf_b(va_list ap, int total);
-int printf_h(va_list ap, int total);
-int printf_o(va_list ap, int total);
+int printf_c(va_list ap, int *total);
+int printf_s(va_list ap, int *total);
+int printf_same(va_list ap, int *total);
+int printf_d(va_list ap, int *total);
+int printf_u(va_list ap, int *total);
+int printf_b(va_list ap, int *total);
+int printf_h(va_list ap, int *total);
+int printf_o(va_list ap, int *total);
 #endif
