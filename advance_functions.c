65d4069fd46cf063479bf048caa76916ca258978
Diego Bonora <bonoradi@gmail.com>
2023-03-31 06:32:39 -0700

    added new function for hexadecimal numbers

--- advance_functions.c
+++ advance_functions.c
@@ -21,16 +21,11 @@ int printf_b(va_list ap, int *total)
 	{
 		str[cant] = ((num % 2) + '0');
 		num /= 2;
-		if (!str)
-		{
-			free(str);
-			return (-1);
-		}
 		cant++;
 	}
 	print_rev(str);
 	free(str);
-	total += cant;
+	*total += cant;
 	return (0);
 }
 
@@ -65,19 +60,49 @@ int printf_h(va_list ap, int *total)
 		{
 			str[cant] = new_num + '0';
 		}
-		if (!str)
+		num /= 16;
+		cant++;
+	}
+	print_rev(str);
+	free(str);
+	*total += cant;
+	return (0);
+}
+
+int printf_H(va_list ap, int *total)
+{
+	unsigned int num;
+	unsigned int new_num;
+	int cant = 0;
+	char diff[] = {'a', 'b', 'c', 'd', 'e', 'f'};
+	char *str;
+
+	str = malloc(sizeof(char) * 64);
+	if (!str)
+		return (-1);
+	num = va_arg(ap, unsigned int);
+	while (num > 0)
+	{
+		new_num = num % 16;
+		if (new_num > 9)
 		{
-			free(str);
-			return (-1);
+			new_num -= 10;
+			str[cant] = diff[new_num];
+		}
+		else
+		{
+			str[cant] = new_num + '0';
 		}
 		num /= 16;
 		cant++;
 	}
 	print_rev(str);
 	free(str);
-	total += cant;
+	*total += cant;
 	return (0);
 }
+
+
 
 /**
  * printf_o - prints an octal number
@@ -100,15 +125,10 @@ int printf_o(va_list ap, int *total)
 	{
 		str[cant] = ((num % 8) + '0');
 		num /= 8;
-		if (!str)
-		{
-			free(str);
-			return (-1);
-		}
 		cant++;
 	}
 	print_rev(str);
 	free(str);
-	total += cant;
+	*total += cant;
 	return (0);
 }
